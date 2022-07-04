1f52a14a7c02979f33f0d6e7712c33aa520b6776
syntheticmagus <33846034+syntheticmagus@users.noreply.github.com>
2022-07-04 12:26:10 -0700

    Semi-tested skeleton.

--- server.js
+++ server.js
@@ -1,13 +1,65 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
-// TODO: This could be a database later, if needed.
-const siteToPeers = new Map();
-
+app.use(cors());
 app.use(express.json());
 
+// TODO: This could be a database later, if needed.
+const siteToPeerIdToTimes = new Map();
+
 app.get("/", function (request, response) {
-    response.send("Successful response.");
+    response.redirect("/status");
+});
+
+app.get("/status", function (request, response) {
+    response.send(`Service live. Active sites: ${siteToPeerIdToTimes.size}`);
+});
+
+app.post("/sensor", function (request, response) {
+    const site = request.body.site;
+    const peerId = request.body.peerId;
+
+    if (!siteToPeerIdToTimes.has(site)) {
+        siteToPeerIdToTimes.set(site, new Map());
+    }
+
+    const peerIdToTime = siteToPeerIdToTimes.get(site);
+    peerIdToTime.set(peerId, Date.now());
+
+    response.status(200).send();
+});
+
+app.post("/viewer", function (request, response) {
+    const site = request.body.site;
+
+    if (!siteToPeerIdToTimes.has(site)) {
+        response.status(404).send();
+        return;
+    }
+
+    const now = Date.now();
+    const peerIdToTime = siteToPeerIdToTimes.get(site);
+    const current = [];
+    const outdated = [];
+    peerIdToTime.forEach((time, peerId) => {
+        if (now - time < 120 * 1000) {
+            current.push(peerId);
+        } else {
+            outdated.push(peerId);
+        }
+    });
+
+    outdated.forEach((peerId) => {
+        peerIdToTime.delete(peerId);
+    });
+
+    if (current.length === 0) {
+        siteToPeerIdToTimes.delete(site);
+        response.status(404).send();
+    } else {
+        response.status(200).send(JSON.stringify({ peerIds: current }));
+    }
 });
 
 const port = process.env.PORT || 3000;
