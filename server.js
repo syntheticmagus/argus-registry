7d6528dfa2131448b0afebdec3897b8d5636c6b8
syntheticmagus <33846034+syntheticmagus@users.noreply.github.com>
2022-07-04 11:33:53 -0700

    Debugging.

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const app = express();
-const server = require("https").Server(app);
 
 // TODO: This could be a database later, if needed.
 const siteToPeers = new Map();
 
-app.use(express.json());
-
 app.get("/", function (request, response) {
-    response.status(404).send("Home page unavailable.");
+    response.send("Successful response.");
 });
 
-server.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
+app.listen(port, () => { console.log(`Listening on port ${port}.`) });
